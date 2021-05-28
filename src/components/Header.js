2dab907c731041591e6a05dab357e4643fc4aad6
starlord <shekharsahani0987@gmail.com>
2021-05-28 20:40:18 +0530

    third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Button from "./Button";
 import PropTypes from "prop-types";
-
+import { useLocation } from "react-router-dom";
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title} </h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? " close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? " close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
